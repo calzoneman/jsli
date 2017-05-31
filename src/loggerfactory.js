fc89522de192796ce6a851ee768f6efd7f25ee7d
Calvin Montgomery <calzoneman@gmail.com>
2017-05-30 21:39:47 -0700

    Only warn once if no log impl is bound

--- src/loggerfactory.js
+++ src/loggerfactory.js
@@ -1,11 +1,15 @@
 import { Logger } from './logger';
 
-var LoggerImplFactory = null;
+let LoggerImplFactory = null;
+let warned = false;
 
 export function getLogger(loggerName) {
     if (LoggerImplFactory === null) {
-        console.log('WARNING: No logger implementation bound for jsli.  '
-                + 'Log messages will be ignored!');
+        if (!warned) {
+            console.log('WARNING: No logger implementation bound for jsli.  '
+                    + 'Log messages will be ignored!');
+            warned = true;
+        }
         return new Logger(null, loggerName);
     }
 
